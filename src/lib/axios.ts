deed2df4c823e58326ef05ee29e2ee471c8514b6
yennie <l.yennie.j@gmail.com>
2025-07-15 01:28:57 +0900

    fix(error):빌드 오류

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios 기본 설정
 const apiClient = axios.create({
-  baseURL: import.meta.env.DEV ? '' : import.meta.env.VITE_API_URL,
+  baseURL: '',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
