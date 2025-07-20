d96a4c20c978771b4323bd1e42545bd2ba03e60a
yennie <l.yennie.j@gmail.com>
2025-07-21 02:08:20 +0900

    change env

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@
 export const env = {
   APP_TITLE: import.meta.env.VITE_APP_NAME || '주차 도우미',
   API_BASE_URL:
-    import.meta.env.VITE_API_URL || import.meta.env.VITE_API_BASE_URL,
+    import.meta.env.API_BASE_URL || 'https://parkinghelp.onrender.com',
   DEBUG_MODE: import.meta.env.VITE_DEBUG_MODE === 'true',
   IS_DEVELOPMENT: import.meta.env.DEV,
 };
